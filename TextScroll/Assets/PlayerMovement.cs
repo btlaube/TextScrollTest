8456c709d2c267e10f9886dca85b1e7f489b3425
Ben Laube <blaube2@huskers.unl.edu>
2021-12-17 17:38:10 -0600

    made Gus and Sasquatch

---
+++ TextScroll/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D rb;
+    //public Animator animator;
+    Vector2 movement;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+
+        //animator.SetFloat("Horizontal", movement.x);
+        //animator.SetFloat("Speed", movement.x);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
